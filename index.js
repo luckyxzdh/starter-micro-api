23e212f4bc8eb79fae2e84168ad9572fd6148e56
luckyxzdh <122589759+luckyxzdh@users.noreply.github.com>
2023-12-24 12:47:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const crypto = require('crypto');
 const fetch = require('node-fetch');
 
@@ -30,87 +31,78 @@ async function hmacSha256Sign(data, expire) {
   return base64.replace(/\+/g, "-").replace(/\//g, "_") + ":" + expire;
 }
 
-async function handleDownload(request) {
-  const origin = request.headers.origin || "*";
-  const url = new URL(request.url);
+const server = http.createServer(async (req, res) => {
+  const origin = req.headers.origin || "*";
+  const url = new URL(req.url, `http://${req.headers.host}`);
   const path = decodeURIComponent(url.pathname);
   const sign = url.searchParams.get("sign") || "";
-  const verifyResult = await verify(path, sign);
-  if (verifyResult !== "") {
-    const resp2 = new Response(
-      JSON.stringify({
-        code: 401,
-        message: verifyResult
-      }),
-      {
-        headers: {
-          "content-type": "application/json;charset=UTF-8"
-        }
-      }
-    );
-    resp2.headers.set("Access-Control-Allow-Origin", origin);
-    return resp2;
-  }
-  let resp = await fetch(`${ADDRESS}/api/fs/link`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-      Authorization: TOKEN
-    },
-    body: JSON.stringify({
-      path
-    })
-  });
-  let res = await resp.json();
-  if (res.code !== 200) {
-    return new Response(JSON.stringify(res));
-  }
-  request = new Request(res.data.url, request);
-  request = new Request(request, { redirect: "follow" });
-  if (res.data.header) {
-    for (const k in res.data.header) {
-      for (const v of res.data.header[k]) {
-        request.headers.set(k, v);
-      }
-    }
-  }
-  let response = await fetch(request);
-  response = new Response(response.body, response);
-  response.headers.delete("set-cookie");
-  response.headers.set("Access-Control-Allow-Origin", origin);
-  response.headers.append("Vary", "Origin");
-  return response;
-}
 
-function handleOptions(request) {
-  const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-    "Access-Control-Max-Age": "86400"
-  };
-  let headers = request.headers;
-  if (headers.get("Origin") !== null && headers.get("Access-Control-Request-Method") !== null) {
-    let respHeaders = {
-      ...corsHeaders,
-      "Access-Control-Allow-Headers": headers.get("Access-Control-Request-Headers") || ""
+  if (req.method === "OPTIONS") {
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Access-Control-Max-Age": "86400"
     };
-    return new Response(null, {
-      headers: respHeaders
-    });
-  } else {
-    return new Response(null, {
-      headers: {
-        Allow: "GET, HEAD, POST, OPTIONS"
-      }
-    });
-  }
-}
-
-module.exports = {
-  async fetch(request, env, ctx) {
-    if (request.method === "OPTIONS") {
-      return handleOptions(request);
+    let headers = req.headers;
+    if (headers.get("Origin") !== null && headers.get("Access-Control-Request-Method") !== null) {
+      let respHeaders = {
+        ...corsHeaders,
+        "Access-Control-Allow-Headers": headers.get("Access-Control-Request-Headers") || ""
+      };
+      res.writeHead(200, respHeaders);
+      res.end();
+    } else {
+      res.writeHead(200, { Allow: "GET, HEAD, POST, OPTIONS" });
+      res.end();
     }
-    return handleDownload(request);
+  } else {
+    const verifyResult = await verify(path, sign);
+    if (verifyResult !== "") {
+      res.writeHead(401, { "content-type": "application/json;charset=UTF-8", "Access-Control-Allow-Origin": origin });
+      res.end(JSON.stringify({ code: 401, message: verifyResult }));
+      return;
+    }
+
+    let fetchRes = await fetch(`${ADDRESS}/api/fs/link`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+        Authorization: TOKEN
+      },
+      body: JSON.stringify({
+        path
+      })
+    });
+
+    let fetchJson = await fetchRes.json();
+    if (fetchJson.code !== 200) {
+      res.writeHead(fetchJson.code, { "content-type": "application/json;charset=UTF-8" });
+      res.end(JSON.stringify(fetchJson));
+      return;
+    }
+
+    let fetchReq = new fetch.Request(fetchJson.data.url, {
+      headers: req.headers,
+      redirect: "follow"
+    });
+
+    let response = await fetch(fetchReq);
+
+    let responseBody = await response.text();
+    let headers = Object.fromEntries(response.headers.entries());
+
+    delete headers['set-cookie'];
+
+    headers["Access-Control-Allow-Origin"] = origin;
+    headers["Vary"] = "Origin";
+
+    res.writeHead(response.status, headers);
+    res.end(responseBody);
   }
-};
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
