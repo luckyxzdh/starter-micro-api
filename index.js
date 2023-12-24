2f07e6cfe46b7ba748b6a8e117c85353907a5fe6
luckyxzdh <122589759+luckyxzdh@users.noreply.github.com>
2023-12-24 12:44:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,116 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const crypto = require('crypto');
+const fetch = require('node-fetch');
+
+const ADDRESS = "https://alist.xzpan.tk";
+const TOKEN = "alist-6fd33570-60be-4737-bb31-bfa41f6dfb52Ej3buinXtVXHlm7cuYYYeZpP91Mx1gx3jSUd356e2T8ki2rbwWi6p6WJI5VgTAnp";
+
+async function verify(data, _sign) {
+  const signSlice = _sign.split(":");
+  if (!signSlice[signSlice.length - 1]) {
+    return "expire missing";
+  }
+  const expire = parseInt(signSlice[signSlice.length - 1]);
+  if (isNaN(expire)) {
+    return "expire invalid";
+  }
+  if (expire < Date.now() / 1e3 && expire > 0) {
+    return "expire expired";
+  }
+  const right = await hmacSha256Sign(data, expire);
+  if (_sign !== right) {
+    return "sign mismatch";
+  }
+  return "";
+}
+
+async function hmacSha256Sign(data, expire) {
+  const key = crypto.createHmac('sha256', Buffer.from(TOKEN));
+  const buf = key.update(`${data}:${expire}`).digest();
+  const base64 = Buffer.from(buf).toString('base64');
+  return base64.replace(/\+/g, "-").replace(/\//g, "_") + ":" + expire;
+}
+
+async function handleDownload(request) {
+  const origin = request.headers.origin || "*";
+  const url = new URL(request.url);
+  const path = decodeURIComponent(url.pathname);
+  const sign = url.searchParams.get("sign") || "";
+  const verifyResult = await verify(path, sign);
+  if (verifyResult !== "") {
+    const resp2 = new Response(
+      JSON.stringify({
+        code: 401,
+        message: verifyResult
+      }),
+      {
+        headers: {
+          "content-type": "application/json;charset=UTF-8"
+        }
+      }
+    );
+    resp2.headers.set("Access-Control-Allow-Origin", origin);
+    return resp2;
+  }
+  let resp = await fetch(`${ADDRESS}/api/fs/link`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+      Authorization: TOKEN
+    },
+    body: JSON.stringify({
+      path
+    })
+  });
+  let res = await resp.json();
+  if (res.code !== 200) {
+    return new Response(JSON.stringify(res));
+  }
+  request = new Request(res.data.url, request);
+  request = new Request(request, { redirect: "follow" });
+  if (res.data.header) {
+    for (const k in res.data.header) {
+      for (const v of res.data.header[k]) {
+        request.headers.set(k, v);
+      }
+    }
+  }
+  let response = await fetch(request);
+  response = new Response(response.body, response);
+  response.headers.delete("set-cookie");
+  response.headers.set("Access-Control-Allow-Origin", origin);
+  response.headers.append("Vary", "Origin");
+  return response;
+}
+
+function handleOptions(request) {
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+    "Access-Control-Max-Age": "86400"
+  };
+  let headers = request.headers;
+  if (headers.get("Origin") !== null && headers.get("Access-Control-Request-Method") !== null) {
+    let respHeaders = {
+      ...corsHeaders,
+      "Access-Control-Allow-Headers": headers.get("Access-Control-Request-Headers") || ""
+    };
+    return new Response(null, {
+      headers: respHeaders
+    });
+  } else {
+    return new Response(null, {
+      headers: {
+        Allow: "GET, HEAD, POST, OPTIONS"
+      }
+    });
+  }
+}
+
+module.exports = {
+  async fetch(request, env, ctx) {
+    if (request.method === "OPTIONS") {
+      return handleOptions(request);
+    }
+    return handleDownload(request);
+  }
+};
